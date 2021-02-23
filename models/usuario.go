952762c40afcf2cb898d13731ea3c501b8a6c33c
Fernandez <pablitopak@gmail.com>
2021-02-23 11:19:45 -0500

    Registro usuario

--- models/usuario.go
+++ models/usuario.go
@@ -18,5 +18,5 @@ type Usuario struct {
 	Banner          string             `bson:"banner" json:"banner,omitempty"`
 	Biografia       string             `bson:"biografia" json:"biografia,omitempty"`
 	Ubicacion       string             `bson:"ubicacion" json:"ubicacion,omitempty"`
-	SitioWeb        string             `bson:"sitioweb" json:"emaisitioweb,omitempty"`
+	SitioWeb        string             `bson:"sitioweb" json:"sitioweb,omitempty"`
 }
