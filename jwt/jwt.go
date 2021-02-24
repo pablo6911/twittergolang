1700979acc2d27ce9eed0620f088c23b84706243
Fernandez <pablitopak@gmail.com>
2021-02-23 19:45:38 -0500

    Login Usuario

---
+++ jwt/jwt.go
@@ -0,0 +1,32 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/pablo6911/twittergolang/models"
+)
+
+//GeneroJWT genera el encriptado con JWT
+func GeneroJWT(t models.Usuario) (string, error) {
+
+	miClave := []byte("twitter-golang")
+
+	payload := jwt.MapClaims{
+		"email":            t.Email,
+		"nombre":           t.Nombre,
+		"apellido":         t.Apellido,
+		"fecha_nacimiento": t.FechaNacimiento,
+		"biografia":        t.Biografia,
+		"ubicacion":        t.Ubicacion,
+		"sitioweb":         t.SitioWeb,
+		"_id":              t.ID.Hex(),
+		"exp":              time.Now().Add(time.Hour * 24).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	tokenstr, err := token.SignedString(miClave)
+	if err != nil {
+		return tokenstr, err
+	}
+	return tokenstr, nil
+}
